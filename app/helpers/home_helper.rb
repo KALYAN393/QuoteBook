aec8f546944c95ff273f19a0b1d595bbf8bdca1c
Kalyan <test@plansource.com>
2022-07-22 12:53:06 +0530

    Adding styling to the views

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,6 +1,7 @@
 module HomeHelper
 
   def post_liked_by_user? post_id
-    Like.where(post_id: post_id, User_id: current_user.id).any?
+    Like.where(Post_id: post_id, User_id: current_user.id).any?
   end
+  
 end
